a3a0017e8627ab5083a7d8c18206998b81904d8d
smarkle <smarkle@amazon.com>
2020-01-19 12:11:29 -0800

    Sierpinksi triangle

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use draw::{render, SvgRenderer, Shape, Drawing, Canvas, Style, RGB, Color};
+use rand::{thread_rng, Rng};
+
+fn dot(x: f32, y: f32, canvas: &mut Canvas) {
+    let dot = Drawing::new().with_shape(Shape::Circle { radius: 1 }).with_xy(x, y)
+        .with_style(Style::filled(Color::black()));
+    canvas.display_list.add(dot);
+}
+
+fn main() {
+
+    let size = 800.0_f32;
+
+    let mut canvas = Canvas::new(size as u32, size as u32);
+
+    let peak = size - ((size * size) - (size/2.0)*(size/2.0)).sqrt();
+
+    let points = vec![(0.0, size ), (size , size ), (size/2.0, peak)];
+
+    for point in &points {
+        dot(point.0, point.1, &mut canvas);
+    }
+
+    let idx = thread_rng().gen_range(0, points.len());
+    let mut point = points[idx];
+    for _ in 1..100000 {
+        let next = points[thread_rng().gen_range(0, points.len())];
+        point = ((point.0 + next.0)/3.0, (point.1 + next.1)/3.0);
+        dot(point.0, point.1, &mut canvas);
+    }
+
+    // save as an svg
+    render::save(&canvas, "tests/svg/sierpinksi.svg", SvgRenderer::new()).expect("Failed to save")
+
+}
